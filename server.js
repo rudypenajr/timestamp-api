1e693f07f70234598b8e7bece6a224381966d226
Rudy Pena <rudy@bitfusion.com>
2018-07-20 21:19:35 -0500

    resolving port issues on heroku

--- server.js
+++ server.js
@@ -34,4 +34,4 @@ app.get('/api/timestamp/:date', (req, res, next) => {
     })
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
